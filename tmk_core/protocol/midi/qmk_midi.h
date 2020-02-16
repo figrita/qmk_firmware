9f5689fecaba76700e4d2ade0c5104bc374be7ad
William Hofferbert <will.hbert@gmail.com>
2020-02-15 22:07:11 -0500

    includes to fix build issues with MIDI_EventPacket_t

--- tmk_core/protocol/midi/qmk_midi.h
+++ tmk_core/protocol/midi/qmk_midi.h
@@ -1,7 +1,16 @@
 #pragma once
 
 #ifdef MIDI_ENABLE
-#    include "midi.h"
+#include "midi.h"
+#include <LUFA/Drivers/USB/USB.h>
+#include "qmk_midi.h"
+#include "sysex_tools.h"
+#include "midi.h"
+#include "usb_descriptor.h"
+#include "process_midi.h"
+#if API_SYSEX_ENABLE
+#    include "api.h"
+#endif
 extern MidiDevice midi_device;
 void              setup_midi(void);
 void              send_midi_packet(MIDI_EventPacket_t* event);
