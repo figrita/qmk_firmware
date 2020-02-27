a42d4ba4c1ccb278b3337f0752a08396dfdc0919
William Hofferbert <will.hbert@gmail.com>
2020-02-26 21:05:45 -0500

    ios/config based power approach

--- keyboards/cannonkeys/ortho75/config.h
+++ keyboards/cannonkeys/ortho75/config.h
@@ -61,6 +61,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PIN_WS2812      15
 #define WS2812_SPI SPID2
 
+#if defined(RGBLIGHT_ENABLE) && !defined(IOS_DEVICE_ENABLE)
+// USB_MAX_POWER_CONSUMPTION value
+//  120  RGBoff
+//  330  RGB 6
+//  300  RGB 32
+  #define USB_MAX_POWER_CONSUMPTION 400
+#else
+  // fix iPhone and iPad power adapter issue
+  // iOS device need lessthan 100
+  #define USB_MAX_POWER_CONSUMPTION 100
+#endif
 
 /*
  * Feature disable options
